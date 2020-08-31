26f58a01b60853ad45d9b791c1d4788bb2c6e7b5
mwanikphili <66971270+mwanikphili@users.noreply.github.com>
2020-08-31 11:55:45 +0300

    Service worker for firebase push notifications
    Ids and Keys reducted....
    Next add notification "Onclick" functionality.

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,32 @@
 
+importScripts('https://www.gstatic.com/firebasejs/7.16.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.16.0/firebase-messaging.js');
+  var firebaseConfig = {
+    apiKey: "",
+    authDomain: "skymilk-1549269219057.firebaseapp.com",
+    databaseURL: "https://skymilk-1549269219057.firebaseio.com",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "1:471847861263:web:f3523813ba068da235b77a",
+    measurementId: "G-5WS57EVFYD"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+const messaging =firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload){
+	const title =payload.notification.title;
+	const options ={
+		body: ,
+		icon:'',
+		vibrate:,
+		data:{
+			dateOfArrival: Date.now(),
+			primaryKey: 'notif-',
+			url: payload.data.url
+			}
+	};
+	
+	return self.registration.showNotification(title, options);
+});
